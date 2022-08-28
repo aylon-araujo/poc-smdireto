90ad2d92e04422a29be6eb6254b8e91b28680b3c
Aylon Araújo <Aylon@MacBook-Air-2020-Carlos-Magno.local>
2022-08-28 17:20:48 -0300

    fix: inprovement sidenav models

--- src/models/components/LayoutModels.tsx
+++ src/models/components/LayoutModels.tsx
@@ -2,10 +2,10 @@ import { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 
 export interface AppBarProps extends MuiAppBarProps {
   open: boolean;
-  itemOpen?: Object | undefined;
 }
 
 export interface LayoutProps {
+  itemOpen: Object;
   isMobile: boolean;
   handleDrawerOpen: () => void;
   handleDrawerClose: () => void;
