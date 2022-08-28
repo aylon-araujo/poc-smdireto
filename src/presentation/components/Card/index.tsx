1fefe81152df48a166c20903949b23dfca8a52d9
Aylon Araújo <Aylon@MacBook-Air-2020-Carlos-Magno.local>
2022-08-28 17:21:50 -0300

    feat: card component

---
+++ src/presentation/components/Card/index.tsx
@@ -0,0 +1,23 @@
+import { Card, CardContent, Typography } from "@mui/material";
+
+interface CardProps {
+  title?: string;
+  content: React.ReactNode | string;
+}
+
+const CardContainer = ({ title, content }: CardProps) => {
+  return (
+    <>
+      <Card>
+        <CardContent>
+          <Typography variant="inherit" align="center">
+            {title}
+          </Typography>
+          {content}
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default CardContainer;
